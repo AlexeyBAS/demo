1e5f0878f7433e46ee7753274d9a0fa77dcbf70b
AlexeyBAS <alexey_366@mail.ru>
2023-02-28 09:03:12 +0400

    commit5

--- Src/Main.java
+++ Src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     private static int a;
     private static int b;
     public static void main(String[] args) {
-
+        int x;
+        int y;
     }
 
     public void step() {
